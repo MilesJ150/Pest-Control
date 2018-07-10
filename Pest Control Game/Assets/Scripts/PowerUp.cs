c271ce5857c6d5b8561fe27b516914817fb012e5
MilesJ150 <miles.johnson1996@gmail.com>
2018-07-10 15:56:21 -0400

    Test

--- Pest Control Game/Assets/Scripts/PowerUp.cs
+++ Pest Control Game/Assets/Scripts/PowerUp.cs
@@ -6,7 +6,7 @@ public class PowerUp : MonoBehaviour {
 
     void OnTriggerEnter(Collider c)
     {
-        if (/*c.attachedRigidbody != null &&*/ c.GetComponent<PowerUpCollector>() != null)
+        if (c.attachedRigidbody != null && c.GetComponent<PowerUpCollector>() != null)
         {
             PowerUpCollector puc = c.attachedRigidbody.gameObject.GetComponent<PowerUpCollector>();
             Destroy(this.gameObject);
