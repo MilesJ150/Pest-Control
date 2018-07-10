7a1b03d2ee63a980e43d62b45a558bd3a5d221b1
MilesJ150 <miles.johnson1996@gmail.com>
2018-07-09 23:26:15 -0400

    Added drone and power-up

---
+++ Pest Control Game/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (/*c.attachedRigidbody != null &&*/ c.GetComponent<PowerUpCollector>() != null)
+        {
+            PowerUpCollector puc = c.attachedRigidbody.gameObject.GetComponent<PowerUpCollector>();
+            Destroy(this.gameObject);
+            puc.ReceivePU();
+        }
+    }
+}
