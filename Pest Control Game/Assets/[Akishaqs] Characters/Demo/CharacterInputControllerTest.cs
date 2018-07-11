ae6c581d3abfb1d455a869a68606091d6a29fe45
EllaKim0415 <khrstar7979@gmail.com>
2018-07-11 18:24:29 -0400

    collectable ball
    after collecting -> runs the whole time

--- Pest Control Game/Assets/[Akishaqs] Characters/Demo/CharacterInputControllerTest.cs
+++ Pest Control Game/Assets/[Akishaqs] Characters/Demo/CharacterInputControllerTest.cs
@@ -2,65 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterInputControllerTest : MonoBehaviour {
+public class CharacterInputControllerTest : MonoBehaviour
+{
 
     static Animator anim;
     public float v;
     public float h;
+    public bool hasPU;
+    public float runTimeCount;
+    public bool reseted = false;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         v = Input.GetAxis("Vertical");
         h = Input.GetAxis("Horizontal");
-        if (v > 0)
+        hasPU = GetComponent<PowerUpCollector>().hasPU;
+        if (hasPU)
         {
-            if (Input.GetKey(KeyCode.LeftShift) && v != 0)
-            {
-                anim.SetBool("isRunning 0", true);
-                anim.SetBool("isWalking", false);
-            } else if (!Input.GetKey(KeyCode.LeftShift) && v != 0)
-            {
-                anim.SetBool("isWalking", true);
-                anim.SetBool("isRunning 0", false);
-            }
-        } else
-        {
-            anim.SetBool("isWalking", false);
+            runTimeCount = 5;
         }
-        if (v < 0)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                anim.SetTrigger("runningleft");
-            } else
-            {
-                anim.SetTrigger("left");
-            }
-        }
-        if (v > 0)
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                anim.SetTrigger("runningright");
-            } else
-            {
-                anim.SetTrigger("right");
-            }
-        }
-        if (v == 0)
-        {
-            anim.SetBool("isIdle", true);
-        } else
+        if (v != 0)
         {
             anim.SetBool("isIdle", false);
-        }
+            if (v > 0)
+            {
+                if (runTimeCount > 0 && v > 0)
+                {
+                    //RUNNING
+                    anim.SetBool("isRunning 0", true);
+                    anim.SetBool("isWalking", false);
+                    if (h < 0)
+                    {
+                        anim.SetBool("runningleft", true);
+                        anim.SetBool("runningright", false);
+                        anim.SetBool("right", false);
+                        anim.SetBool("left", false);
+                    }
+                    else if (h > 0)
+                    {
+                        anim.SetBool("runningright", true);
+                        anim.SetBool("runningleft", false);
+                        anim.SetBool("right", false);
+                        anim.SetBool("left", false);
+                    } else
+                    {
+                        anim.SetBool("runningleft", false);
+                        anim.SetBool("right", false);
+                        anim.SetBool("left", false);
+                        anim.SetBool("runningright", false);
+                    }
+                }
+                else if (runTimeCount <= 0 && v > 0)
+                {
+                    reseted = true;
+                    //WALKING
+                    anim.SetBool("isRunning 0", false);
+                    anim.SetBool("isWalking", true);
+                    if (h < 0)
+                    {
+                        anim.SetBool("left", true);
+                        anim.SetBool("right", false);
+                        anim.SetBool("runningright", false);
+                        anim.SetBool("runningleft", false);
+                    }
+                    else if (h > 0)
+                    {
+                        anim.SetBool("right", true);
+                        anim.SetBool("left", false);
+                        anim.SetBool("runningright", false);
+                        anim.SetBool("runningleft", false);
+                    } else
+                    {
+                        anim.SetBool("runningleft", false);
+                        anim.SetBool("right", false);
+                        anim.SetBool("left", false);
+                        anim.SetBool("runningright", false);
+                    }
+                } else if (v == 0)
+                {
+                    anim.SetBool("isIdle", true);
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isRunning 0", false);
+                }
 
-	}
+            }
+        } else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isRunning 0", false);
+            anim.SetBool("isWalking", false);
+        }
+        runTimeCount -= Time.time;
+    }
 
     void FixedUpdate()
     {
