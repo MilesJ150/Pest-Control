1c5638f480b121631b168cf8035005cb413fe17c
Dichao Hu <hudichao1998@163.com>
2018-07-11 10:25:42 +0800

    AI stuff added

---
+++ Pest Control Game/Assets/Scripts/VelocityReporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VelocityReporter : MonoBehaviour {
+	private Vector3 prevPos;
+	public Vector3 velocity
+	{
+		get;
+		private set;
+	}
+	public Vector3 prevVelocity
+	{
+		get;
+		private set;
+	}
+	// Use this for initialization
+	void Start () {
+		prevPos = this.transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		velocity = (this.transform.position - prevPos) / Time.deltaTime;
+		if (Vector3.Magnitude(velocity) > 0.2f) {
+			prevVelocity = velocity;
+		}
+		prevPos = this.transform.position;
+	}
+}
