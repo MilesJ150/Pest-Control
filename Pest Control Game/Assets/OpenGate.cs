b83b0b2af0a31cbe15a7423c802d242bca3d2a2d
MilesJ150 <miles.johnson1996@gmail.com>
2018-07-30 16:12:42 -0400

    Gate and pressure plate

---
+++ Pest Control Game/Assets/OpenGate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+
+    public GameObject gate;
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        Destroy(gate);
+    }
+}
